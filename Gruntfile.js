932bb56339da0c88420be79572e236e334060c4e
litmit <litmit@gmail.com>
2013-12-18 19:38:17 +0300

    fix keywords

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt)
          all: [
             "Gruntfile.js",
             "tasks/*.js",
-           // "<%= nodeunit.tests %>",
+            "<%= nodeunit.tests %>",
          ]
       },
 
